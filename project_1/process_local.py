6bb4b265251f3a19106c8542c8f0bdd5b46c1b4c
Kenny Yu <kennyyu168@yahoo.com>
2019-07-15 21:26:47 -0700

    Wrote process_image() function and got the correct transformed picture

--- project_1/process_local.py
+++ project_1/process_local.py
@@ -13,22 +13,38 @@ Now modify this code to process a local image and do the following:
 All the above steps should be in one function called process_image()
 """
 
-# TODO: Import OpenCV
+# Import OpenCV
+import cv2
 
-
-# TODO: Edit this function
+# Edit this function
 def process_image():
-    return
 
-# Just prints 'Hello World! to screen.
-def hello_world():
-    print('Hello World!')
-    return
+    # Reads in the image 
+    image_read = cv2.imread("geisel.jpg")
 
-# TODO: Call process_image function.
+    # Converts the image to greyscale
+    grey = cv2.cvtColor(image_read, cv2.COLOR_RGB2GRAY)
+    
+    # Resizes the image to half it's dimensions
+    width = int(grey.shape[1] * 0.5)
+    height = int(grey.shape[0] * 0.5)
+    dim = (width, height)
+    resized = cv2.resize(grey, dim, interpolation = cv2.INTER_AREA)
+
+    # Draws a box at the center of the image 
+    top_corner_x = ( width / 2 ) - 50
+    top_corner_y = ( height / 2 ) - 50
+    bottom_corner_x = ( width / 2 ) + 50
+    bottom_corner_y = ( height / 2 ) + 50
+    boxed = cv2.rectangle( resized, (top_corner_x, top_corner_y), (bottom_corner_x, bottom_corner_y), (255, 255, 255), 3) 
+
+    # Save image 
+    cv2.imwrite( "geisel-bw-rectangle.jpg", boxed )
+
+
+# Call process_image function.
 def main():
-    hello_world()
-    return
+    process_image()
 
 
 if(__name__ == '__main__'):
